e33863983d11c783149a7a68727298f3303ddefe
nuoki <teslaboysound1111@gmail.com>
2023-01-29 21:59:35 +0300

    Translate messages

--- bot/messages.py
+++ bot/messages.py
@@ -1,11 +1,11 @@
-start_message = 'Привет! Я бот, который переносит стиль с одной фотографии ' \
-    'на другую с помощью нейронной сети. \nЧтобы начать работу со мной, набери команду /style'
+start_message = "Hello! I'm a bot that transfers style from one photo " \
+    "to another with the help of a neural network. \To start working with me, type the command /style"
 
-help_message = 'Вы можете посмотреть пример моей работы, набрав команду /example.\n' + \
-               'Чтобы начать работу, набери команду /style\n' + \
-               'Загрузи свою картинку а после стилевую!' + \
-               ' В ответ Вы получите стилизованную картинку!\n' + \
-               'Вы всегда можете посмотреть эту инфомацию снова, набрав команду /help.'
+help_message = 'You can see an example of my work by typing the command /example.\n' + \
+    'To get started, type the command /style\n' + \
+    'Upload your picture and then a style one!' + \
+    ' In response, you will receive a stylized picture!\n' + \
+    'You can always view this information again by typing /help.'
 
 echo_message = 'Type /style to start, or /help to view the help.'
 
